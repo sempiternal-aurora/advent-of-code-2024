5e62838e515f1f844bb829ff253430601f00d3ea
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2025-05-01 22:30:54 +1000

    do day 5 and 6

--- src/AOC.hs
+++ src/AOC.hs
@@ -4,12 +4,15 @@ import Data.List
 import Control.Arrow
 import Text.ParserCombinators.ReadP
 import Data.Char
+import Data.Tuple
+import Data.Maybe
+import Debug.Trace
 
 data Part = A | B
     deriving (Show, Read, Eq)
 
 functions :: [(String -> Int, String -> Int)]
-functions = [(day1a, day1b), (day2a, day2b), (day3a, day3b), (day4a, day4b)]
+functions = [(day1a, day1b), (day2a, day2b), (day3a, day3b), (day4a, day4b), (day5a, day5b), (day6a, day6b)]
 
 getDay :: Int -> Part -> String -> Int
 getDay n A = fst $ (!!) functions $ n-1
@@ -167,3 +170,129 @@ get3by3 = foldr helper [[]]
         slice n ls@(_:xs) = take n ls : slice n xs
         slice _ []        = []
         
+
+day5a :: String -> Int
+day5a = sum . map middle . (\(rules, updates) -> filter (day5Pred rules) updates) . day5Splitter . break null . lines
+
+day5Pred :: [(Int,Int)] -> [Int] -> Bool
+day5Pred rules = all (uncurry helper) . (predRules rules <*>) . pure
+    where
+        helper :: Int -> (Int, Int) -> Bool
+        helper l (x,y) = l == x || l == y || x <= y
+
+getProblem :: [(Int,Int)] -> [Int] -> (Int, Int)
+getProblem rules = head . mapMaybe (uncurry helper) . (predRules rules <*>) . pure
+    where
+        helper :: Int -> (Int, Int) -> Maybe (Int, Int)
+        helper l (x,y) = if l == x || l == y || x <= y then Nothing else Just (x,y)
+        
+predRules :: [(Int, Int)] -> [[Int] -> (Int, (Int, Int))]
+predRules = map ((length &&&) . predicate')
+
+predicate' :: (Int, Int) -> [Int] -> (Int, Int) 
+predicate' = uncurry (&&&) . both (length .: takeWhile . (/=))
+
+day5Splitter :: ([String], [String]) -> ([(Int, Int)], [[Int]])
+day5Splitter = map ((read *** read . tail) . break (=='|')) *** tail . map (map read . splitWhen (==','))
+
+(.:) :: (c -> d) -> (a -> b -> c) -> (a -> b -> d)
+f .: g = \x y -> f $ g x y
+infixr 8 .:
+
+both :: (a -> b) -> (a,a) -> (b,b)
+both f = f *** f
+
+middle :: [a] -> a
+middle [] = error "No middle of empty list"
+middle [a] = a
+middle [_,_] = error "Even number of elements has no middle"
+middle xs = middle $ tail $ init xs
+
+splitWhen :: (a -> Bool) -> [a] -> [[a]]
+splitWhen _ [] = []
+splitWhen f xs = let
+        (hd, tl) = break f xs
+    in case tl of 
+        [] -> [hd]
+        _:xs' -> hd : splitWhen f xs'
+
+day5b :: String -> Int
+day5b = 
+    sum . 
+    uncurry helper .
+    day5Splitter . 
+    break null . 
+    lines
+    where
+        helper :: [(Int, Int)] -> [[Int]] -> [Int]
+        helper rules = map (middle . helper' rules) . filter (not . day5Pred rules)
+        
+        helper' :: [(Int, Int)] -> [Int] -> [Int]
+        helper' rules ls = if day5Pred rules ls then ls else let 
+                (x,y) = getProblem rules ls 
+            in helper' rules $ swapList x y ls
+
+swapList :: Show a => Int -> Int -> [a] -> [a]
+swapList x y
+    | x < y = uncurry (++) . 
+              second (
+                uncurry (++) . 
+                uncurry helper .
+                ((swap . both head) &&& both tail) . 
+                splitAt (y-x)
+              ) . splitAt x
+    | x == y = id
+    | otherwise = swapList y x
+    where
+        helper :: (a,a) -> ([a], [a]) -> ([a], [a])
+        helper (x',y') (xs,ys) = (x':xs,y':ys)
+
+data Dir = N | S | E | W
+
+nextDir :: Dir -> Dir
+nextDir N = E
+nextDir E = S
+nextDir S = W
+nextDir W = N
+
+replace :: Int -> Int -> a -> [[a]] -> [[a]]
+replace x y a = uncurry (++) . 
+    second (
+        uncurry (:) . first (
+            uncurry (++) .
+            second (
+                uncurry (:) . first (const a) . fromJust . uncons
+            ) .
+            splitAt x
+        ) . fromJust . uncons
+    ) . 
+    splitAt y
+
+day6a :: String -> Int
+day6a s = let
+        grid = lines s
+        rows = length grid
+        cols = length $ head grid
+        guardPos = length $ takeWhile (/='^') $ concat grid
+        guardXY = (guardPos `mod` cols, guardPos `div` rows)
+        next N (x,y) = (x,y-1)
+        next S (x,y) = (x,y+1)
+        next E (x,y) = (x+1,y)
+        next W (x,y) = (x-1,y)
+        inside x y = x >= 0 && x < cols && y >= 0 && y < rows
+        helper :: Dir -> (Int, Int) -> [[Char]] -> Int
+        helper d (x,y) gr = 
+            let
+                (x',y') = next d (x,y)
+                rotate = (gr' !! y' !! x') == '#'
+                d' = if rotate then nextDir d else d
+                (x'', y'') = if rotate then (x,y) else (x',y')
+                gr' = replace x y 'X' gr
+            in 
+                if inside x' y'
+                then helper d' (x'',y'') gr'
+                else (sum . map (length . filter (=='X'))) gr'
+    in helper N guardXY grid
+        
+day6b :: String -> Int
+day6b = undefined
--- test/Main.hs
+++ test/Main.hs
@@ -15,7 +15,13 @@ main = do
 
 
 tests :: Test
-tests = TestList [day1aTest, day1bTest, day2aTest, day2bTest, day3aTest, day3bTest, day4aTest, day4bTest]
+tests = TestList [
+    day1aTest, day1bTest, 
+    day2aTest, day2bTest, 
+    day3aTest, day3bTest, 
+    day4aTest, day4bTest, 
+    day5aTest, day5bTest,
+    day6aTest, day6bTest]
 
 day1TestInput :: String
 day1TestInput = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3"
@@ -31,6 +37,12 @@ day3bTestInput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo(
 
 day4TestInput :: String
 day4TestInput = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX"
+
+day5TestInput :: String
+day5TestInput = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47"
+
+day6TestInput :: String
+day6TestInput = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#..."
 
 day1aTest :: Test
 day1aTest = TestCase $ assertEqual "Simple test for day 1 part a as given" 11 (getDay 1 A day1TestInput)
@@ -56,5 +68,14 @@ day4aTest = TestCase $ assertEqual "Simple test for day 4 part a as given" 18 (g
 day4bTest :: Test
 day4bTest = TestCase $ assertEqual "Simple test for day 4 part b as given" 9 (getDay 4 B day4TestInput)
 
+day5aTest :: Test
+day5aTest = TestCase $ assertEqual "Simple test for day 5 part a as given" 143 (getDay 5 A day5TestInput)
 
+day5bTest :: Test
+day5bTest = TestCase $ assertEqual "Simple test for day 5 part b as given" 123 (getDay 5 B day5TestInput)
 
+day6aTest :: Test
+day6aTest = TestCase $ assertEqual "Simple test for day 6 part a as given" 41 (getDay 6 A day6TestInput)
+
+day6bTest :: Test
+day6bTest = TestCase $ assertEqual "Simple test for day 6 part b as given" 6 (getDay 6 A day6TestInput)
